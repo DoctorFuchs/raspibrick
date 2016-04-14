df1871f0837ea60205e364a34a1e336b73606158
Aegidius <a2015@pluess.name>
2016-04-14 07:11:04 +0200

    Installation files V1.30, April 13, 2016

---
+++ RenameHost.py
@@ -0,0 +1,26 @@
+# RenameHost.py
+
+import sys
+from raspibrick import *
+import os, subprocess
+
+fname = "/mnt/recovery/raspibrick-rename.requested"
+if not os.path.isfile(fname):
+    print "RenameHost tag file not found"
+    sys.exit()
+
+fInp = open(fname)
+hostname = fInp.readline()
+fInp.close()
+print "New host name:", hostname
+robot = Robot()
+display = Display()
+display.showTicker("CHG HOST", 1, 1, True)
+
+subprocess.call("sudo /home/pi/raspibrick/rename-host " + hostname, shell=True)
+os.remove(fname)
+display.showText("boot")
+
+Tools.delay(2000)
+robot.exit()
+os.system("sudo shutdown -r now")
